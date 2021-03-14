6ec01dce60bf281e9c283ac5f07e3e5eca13768a
Danezis <kdanezis@gmail.com>
2021-03-14 18:07:41 +0100

    update dico.js

--- themes/10ko/assets/js/dico.js
+++ themes/10ko/assets/js/dico.js
@@ -1,10 +1,16 @@
-// Some code could be here ...
-var body = document.body.innerHTML
+var body = document.body.parentElement.innerHTML
 var start = body.length
 var dico = new Map()
 var weight = null
 var orders = null
-var g = ["«","»","“","”","ʹ","͵","·",";","Α","Β","Γ","Δ","Ε","Ϛ","Ϝ","Ζ","Η","Θ","Ι","Κ","Λ","Μ","Ν","Ξ","Ο","Π","Ϟ","Ρ","Σ","Τ","Υ","Φ","Χ","Ψ","Ω","Ϡ","α","β","ϐ","γ","δ","ε","ϛ","ϝ","ζ","η","θ","ι","κ","λ","μ","ν","ξ","ο","π","ϟ","ρ","σ","ς","τ","υ","φ","χ","ψ","ω","ϡ"]
+
+//Grec Dictionnary
+//var g = ["«","»","“","”","ʹ","͵","·",";","Α","Β","Γ","Δ","Ε","Ϛ","Ϝ","Ζ","Η","Θ","Ι","Κ","Λ","Μ","Ν","Ξ","Ο","Π","Ϟ","Ρ","Σ","Τ","Υ","Φ","Χ","Ψ","Ω","Ϡ","α","β","ϐ","γ","δ","ε","ϛ","ϝ","ζ","η","θ","ι","κ","λ","μ","ν","ξ","ο","π","ϟ","ρ","σ","ς","τ","υ","φ","χ","ψ","ω","ϡ"]
+
+//Latin Dictionnary (better)
+var g = ["¡","¢","£","¤","¥","¦","§","¨","©","ª","«","¬","®","¯","°","±","²","³","´","µ","¶","·","¸","¹","º","»","¼","½","¾","¿","À","Á","Â","Ã","Ä","Å","Æ","Ç","È","É","Ê","Ë","Ì","Í","Î","Ï","Ð","Ñ","Ò","Ó","Ô","Õ","Ö","×","Ø","Ù","Ú","Û","Ü","Ý","Þ","ß","à","á","â","ã","ä","å","æ","ç","è","é","ê","ë","ì","í","î","ï","ð","ñ","ò","ó","ô","õ","ö","÷","ø","ù","ú","û","ü","ý","þ","ÿ"]
+
+
 let getDic = (str) => {
     let slice = ""
     let word = ""
@@ -24,12 +30,16 @@ let getDic = (str) => {
 }
 
 let getWeigth = (value, key, map) => {
-    weight.set(key, key.length * (value - 1) -9) // -1 car poid du dico, -9 
+    weight.set(key, key.length * (value - 1) -9) // -1 car poids du dico, -9 
 }
 
-var solvetxt = "new Map(["
+var dicoMap = "new Map(["
 
 for(let i = 0; i < g.length; i++){
+    if(body.includes(g[i])){
+        console.info("drop " + g[i])
+        continue
+    }
     weight = new Map()
     dico = getDic(body).forEach(getWeigth)
     orders = new Map([...weight.entries()].sort((a, b) => b[1] - a[1])); 
@@ -37,31 +47,14 @@ for(let i = 0; i < g.length; i++){
     body = body.split(key).join(g[i])
     console.info("replace : `" + key + "` by `" + g[i] + "` for an expected gain of " + value + " octets")
     if(i>0){
-        solvetxt+=","
+        dicoMap+=","
     }
-    solvetxt+="['" + g[i] + "', '" + key + "']"   
+    dicoMap+="['" + g[i] + "', '" + key + "']"   
 }
-solvetxt +="])"
+dicoMap +="])"
 
+body = '<!doctype html><body></body><script>var b=`' + body + '`;var s=' + dicoMap.replaceAll('\n',"\\n") + ';r=(e=>{let l=e.next().value;if(null!=l){let[t,i]=l;r(e),b=b.split(t).join(i)}}),r(s.entries());document.body.parentElement.innerHTML=b</script>'
 
-console.info(start + " - " + body.length)
-console.info(solvetxt)
-
-var b = body
-var s = new Map([['«', 'e '],['»', '><'],['“', 'es'],['”', ' class="'],['ʹ', 'a href="'],['͵', 'on'],['·', ' d'],[';', 'en'],['Α', 'an'],['Β', '</'],['Γ', 'https://'],['Δ', 'er'],['Ε', 'imag'],['Ϛ', '»/'],['Ϝ', 't '],['Ζ', '“ '],['Η', 'ti'],['Θ', 'li'],['Ι', 'spΑ'],['Κ', 'co'],['Λ', 'in'],['Μ', '">'],['Ν', 's '],['Ξ', 'qu'],['Ο', 'ou'],['Π', 'ai'],['Ϟ', 'le'],['Ρ', 'or'],['Σ', 'div'],['Τ', '»ʹΓkdΑezis'],['Υ', 'un'],['Φ', '="'],['Χ', 'l«'],['Ψ', '·«'],['Ω', 'ar'],['Ϡ', 'appel'],['α', ' s'],['β', 'te'],['ϐ', 'Βa'],['γ', '»p>'],['δ', '/imgs/Ϟav“'],['ε', 'de'],['ϛ', 'as'],['ϝ', 'pr'],['ζ', 'hall;g'],['η', 'web'],['θ', 'm;'],['ι', 't«'],['κ', 'ag'],['λ', '»Θ»ʹ'],['μ', 'tr'],['ν', 'la '],['ξ', 'headΔ'],['ο', '"»'],['π', 'Η͵'],['ϟ', 'ur'],['ρ', 'id'],['σ', 'Ι>\n<Ι'],['ς', 'Βp'],['τ', '“-'],['υ', 'po'],['φ', 're'],['χ', 'ϐϚ'],['ψ', ', '],['ω', 'c͵'],['ϡ', 'au']])
-
-
-r=(e=>{let l=e.next().value;if(null!=l){let[t,i]=l;r(e),b=b.split(t).join(i)}}),r(s.entries());
-/*
-r = (i) => {
-    let n = i.next().value
-    if(n != undefined){
-        let [k, v] = n
-        r(i)
-        body = body.split(k).join(v)
-    }
-}
-
-r(s.entries())
-*/
-console.info(start + " - " + b.length)
+console.info(" Before : " + start + " After : " + body.length)
+console.info(" new HTML :")
+console.info(body)
